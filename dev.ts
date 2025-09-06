9a4728e8431964b246f4bc9226719b6d87f19e7b
Nap Joseph Calub <njncalub@gmail.com>
2025-09-06 11:20:55 +0800

    feat: update std lib

--- dev.ts
+++ dev.ts
@@ -3,6 +3,7 @@
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
 
-import "$std/dotenv/load.ts";
+import { load } from "@std/dotenv";
+await load({ export: true });
 
 await dev(import.meta.url, "./main.ts", config);
